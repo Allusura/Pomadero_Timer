6afd1e4d29cd8319a8554e80966bc02e641e06c4
Allusura <110790823+Allusura@users.noreply.github.com>
2024-01-18 18:59:10 +0000

    Update and rename README.md to README.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pomadero_Timer
-A Pomadero Timer made with Python after 28 days of study.
---
+++ README.py
@@ -0,0 +1,13 @@
+#Description:
+# A Python program I learned to make after 28 days of study.
+# The program is a Pomadero counter which is a method of studying with intervals.
+
+# Things I learned today:
+# Canvas Widget and how to use it
+# How to remove the border of a Canvas when the background is a filled colour
+# How to add a Count-Down Mechanism
+# Learned about Dynamic Typing
+# Learned about global variables
+# Learned how to change the text of a canvas,
+# canvas.itemconfig("name", text="00:00")
+# How to insert an image with "canvas"
